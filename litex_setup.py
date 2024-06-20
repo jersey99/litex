d5b54251803c16ab92b008cb708ea680b6549d53
Vamsi Vytla <vamsi.vytla@gmail.com>
2024-06-20 12:25:47 -0700

    Revert "Single depth clone"
    This reverts commit 4a866634d723e188b87ab69be21741c7d317d9b8.

--- litex_setup.py
+++ litex_setup.py
@@ -218,7 +218,7 @@ def litex_setup_init_repos(config="standard", tag=None, dev_mode=False):
                 repo_url = repo_url.replace("https://github.com/", "git@github.com:")
             subprocess.check_call("git clone {url} {options}".format(
                 url     = repo_url + name + ".git",
-                options = "--recursive --depth 1" if repo.clone == "recursive" else "--depth 1"
+                options = "--recursive" if repo.clone == "recursive" else ""
                 ), shell=True)
             os.chdir(os.path.join(current_path, name))
             # Use specific Branch.
