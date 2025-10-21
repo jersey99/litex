c95dc5944536f0ae80c96264f0f915a90d873d99
Vamsi Vytla <vamsi.vytla@gmail.com>
2025-10-21 16:20:57 -0700

    main.c: remove netboot

--- litex/soc/software/bios/main.c
+++ litex/soc/software/bios/main.c
@@ -75,7 +75,7 @@ static void boot_sequence(void)
 #ifdef CSR_ETHPHY_MODE_DETECTION_MODE_ADDR
 	eth_mode();
 #endif
-	netboot(0, NULL);
+	// netboot(0, NULL);
 #endif
 	printf("No boot medium found\n");
 }
